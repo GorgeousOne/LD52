5a2a1e8b02a1446d158c3d72a3a776ea8f419519
IEatSushi <2klaus.4peter@gmail.com>
2023-01-07 21:48:09 +0100

    WIP add people queue

--- Assets/Scripts/PeopleHandler.cs
+++ Assets/Scripts/PeopleHandler.cs
@@ -1,14 +1,18 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PeopleHandler : MonoBehaviour {
-	
+
+	[SerializeField] private Transform bargainPoint;
+	private List<Vector2> _waypoints;
 	
 	private void OnEnable() {
-		
+		Transform[] children = transform.GetComponentsInChildren<Transform>();
+		_waypoints = children.Select(transform1 => (Vector2) transform1.position).ToList();
 	}
 
-	void Update() {}
+	void Update() {
+		
+	}
 }
